93be55f1f5ef6072cacc89d3c9b4f6a710f6b2f1
Lukasz Kazmierczak <lukask84@o2.pl>
2025-02-12 23:59:40 +0100

    Adding core files, ver 2
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -23,3 +23,26 @@ EXTERNALPROJECT_ADD(
 
 include_directories(${source_dir}/inc)
 link_directories(${build_dir}/build)
+
+
+set(source_dir "${CMAKE_BINARY_DIR}/libshellcmd-src")
+set(build_dir "${CMAKE_BINARY_DIR}/libshellcmd-build")
+
+EXTERNALPROJECT_ADD(
+  libshellcmd
+  GIT_REPOSITORY    https://github.com/lukaskaz/lib-shellcmd.git
+  GIT_TAG           main
+  PATCH_COMMAND     ${patching_cmd}
+  PREFIX            libshellcmd-workspace
+  SOURCE_DIR        ${source_dir}
+  BINARY_DIR        ${build_dir}
+  CONFIGURE_COMMAND mkdir /${build_dir}/build &> /dev/null
+  BUILD_COMMAND     cd ${build_dir}/build && cmake -D BUILD_SHARED_LIBS=ON
+                    ${source_dir} && make -j 4
+  UPDATE_COMMAND    ""
+  INSTALL_COMMAND   "" 
+  TEST_COMMAND      ""
+)
+
+include_directories(${source_dir}/inc)
+link_directories(${build_dir}/build)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "graphs/interfaces/dygraph/rangesamples/graph.hpp"
+#include "shellcommand.hpp"
 
 #include <fstream>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std::string_literals;
 
@@ -10,9 +13,9 @@ std::shared_ptr<graphs::GraphIf> getgraph()
 {
     using namespace graphs::dygraph::rangesamples;
     auto graph = graphs::GraphFactory::create<Graph, configall_t>(
-        {{"cpu temperature", "time/date", "temperature["s + "\u2103" + "]"},
+        {{"cpu/gpu temperature", "time/date", "temperature["s + "\u2103" + "]"},
          {1200, 400},
-         {1s, 60, {{"temprecords.csv", "timestamp,temperature", 86400}}}});
+         {1s, 60, {{"temprecords.csv", "timestamp,cputemp,gputemp", 86400}}}});
     graph->start();
     return graph;
 }
@@ -29,23 +32,47 @@ std::string getfileline(const std::string& file)
 
 std::string getcputemp()
 {
-    static constexpr auto cputempfile{"/sys/class/thermal/thermal_zone0/temp"s};
-    double value = std::atoi(getfileline(cputempfile).c_str()) / 1000.f;
-    std::stringstream ss;
-    ss << std::fixed << std::setprecision(2) << value;
-    return ss.str();
+    static const auto cputempfile{"/sys/class/thermal/thermal_zone0/temp"s};
+    auto reading = getfileline(cputempfile);
+    if (!reading.empty())
+    {
+        double value = std::atoi(reading.c_str()) / 1000.f;
+        std::stringstream ss;
+        ss << std::fixed << std::setprecision(2) << value;
+        return ss.str();
+    }
+    throw std::runtime_error("Cannot get CPU temp");
+}
+
+std::string getgputemp()
+{
+    static const auto gputempcmd{
+        "vcgencmd measure_temp|sed -r \"s/temp=(.*)'C/\\1/g\""s};
+    std::vector<std::string> resultvect;
+    shell::BashCommand().run(gputempcmd, resultvect);
+    if (!resultvect.empty())
+        return resultvect.at(0);
+    throw std::runtime_error("Cannot get GPU temp");
 }
 
 int main(int argc, [[maybe_unused]] char** argv)
 {
     if (argc == 1)
     {
-        auto graph{getgraph()};
-        auto tm{graphs::helpers::TimeMonitor()};
-        while (true)
+        try
         {
-            graph->add(tm.gettimestamp() + "," + getcputemp());
-            usleep(1000 * 1000);
+            auto graph{getgraph()};
+            auto tm{graphs::helpers::TimeMonitor()};
+            while (true)
+            {
+                graph->add(tm.gettimestamp() + "," + getcputemp() + "," +
+                           getgputemp());
+                usleep(1000 * 1000);
+            }
+        }
+        catch (const std::exception& err)
+        {
+            std::cerr << "[ERROR] " << err.what() << '\n';
         }
     }
     return 0;
