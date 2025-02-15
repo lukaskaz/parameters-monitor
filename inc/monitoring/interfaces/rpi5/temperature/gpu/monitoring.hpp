aede35c9e81a5b16d5d2ea7ad7ada438003a488d
lukaskaz <lukask84@o2.pl>
2025-02-15 22:17:00 +0100

    Refactoring and adding composite to handle parameters as group (#2)
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

---
+++ inc/monitoring/interfaces/rpi5/temperature/gpu/monitoring.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "monitoring/factory.hpp"
+
+#include <string>
+#include <variant>
+
+namespace monitoring::rpi5::temperature::gpu
+{
+
+using config_t = std::variant<std::monostate, std::string>;
+
+class Monitoring : public MonitoringIf
+{
+  public:
+    ~Monitoring();
+    std::string reading() const override;
+
+  private:
+    friend class monitoring::Factory;
+    Monitoring(const config_t&);
+
+    struct Handler;
+    std::unique_ptr<Handler> handler;
+};
+
+} // namespace monitoring::rpi5::temperature::gpu
